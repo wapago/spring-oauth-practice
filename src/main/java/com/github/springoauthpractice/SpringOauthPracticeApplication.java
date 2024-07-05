b75ccae676a0b469b2f73b4684a5ef194181f077
Kim Chanho <beroso20th@gmail.com>
2024-07-06 04:55:04 +0900

    first commit

---
+++ src/main/java/com/github/springoauthpractice/SpringOauthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.springoauthpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthPracticeApplication.class, args);
+    }
+
+}
