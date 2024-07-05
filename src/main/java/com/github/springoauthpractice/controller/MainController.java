40021da061e741d979fb98b6233d81981606be7b
Kim Chanho <beroso20th@gmail.com>
2024-07-06 05:10:15 +0900

    Chore: 컨트롤러

--- src/main/java/com/github/springoauthpractice/controller/MainController.java
+++ src/main/java/com/github/springoauthpractice/controller/MainController.java
@@ -1,2 +1,16 @@
-package com.github.springoauthpractice.controller;public class MainController {
+package com.github.springoauthpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainAPI() {
+
+        return "main route";
+    }
 }
--- src/main/java/com/github/springoauthpractice/controller/MyController.java
+++ src/main/java/com/github/springoauthpractice/controller/MyController.java
@@ -1,2 +1,16 @@
-package com.github.springoauthpractice.controller;public class MyController {
+package com.github.springoauthpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my")
+    @ResponseBody
+    public String myAPI() {
+
+        return "my route";
+    }
 }
