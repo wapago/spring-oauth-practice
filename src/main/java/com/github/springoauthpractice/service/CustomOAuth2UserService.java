51e87c903409a6a1ce985c904d9d88b01ea26d3f
Kim Chanho <beroso20th@gmail.com>
2024-07-09 22:47:46 +0900

    Feat: Response추가

---
+++ src/main/java/com/github/springoauthpractice/service/CustomOAuth2UserService.java
@@ -0,0 +1,39 @@
+package com.github.springoauthpractice.service;
+
+import com.github.springoauthpractice.response.GoogleResponse;
+import com.github.springoauthpractice.response.NaverResponse;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationResponse;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        System.out.println(oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2AuthorizationResponse oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        }
+        else {
+
+            return null;
+        }
+
+        //추후 작성
+    }
+}
